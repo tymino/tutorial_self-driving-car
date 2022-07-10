b4ade4682189c7ce386bf4d160b7a75ba73145d0
tymino <d_team-lex@bk.ru>
2022-07-10 18:43:12 +0300

    road move

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ canvas.width = 200;
 const ctx = canvas.getContext('2d');
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(100, 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -14,8 +14,13 @@ function animate() {
 
   canvas.height = window.innerHeight;
 
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.5);
+
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
 
   requestAnimationFrame(animate);
 }
