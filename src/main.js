e8b70998f6d38bb4a2f138d07e143c98509b7491
tymino <d_team-lex@bk.ru>
2022-07-12 11:59:45 +0300

    traffic

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ canvas.width = 200;
 const ctx = canvas.getContext('2d');
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'KEYS');
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
 
 animate();
 
 function animate() {
-  car.update(road.borders);
+  traffic.forEach((car) => car.update(road.borders, []));
+
+  car.update(road.borders, traffic);
 
   canvas.height = window.innerHeight;
 
@@ -18,11 +21,10 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.5);
 
   road.draw(ctx);
-  car.draw(ctx);
+  traffic.forEach((car) => car.draw(ctx, 'red'));
+  car.draw(ctx, 'blue');
 
   ctx.restore();
 
   requestAnimationFrame(animate);
 }
-
-// 1:24:16
